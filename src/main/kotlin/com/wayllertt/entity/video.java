656c48f14f35a21fd05490d4a9c0590d208f9f5b
Aitana <alidzhieva2005@mail.ru>
2025-05-10 17:46:15 +0300

    что-то сомнительное

---
+++ src/main/kotlin/com/wayllertt/entity/video.java
@@ -0,0 +1,37 @@
+package com.wayllertt.entity;
+
+import org.intellij.lang.annotations.Identifier;
+import java.time.LocalDateTime ; // Для работы с датой/временем
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.samples.petclinic.model.Entity;
+import org.springframework.samples.petclinic.model.Table;
+import org.springframework.samples.petclinic.model.Id;
+import org.springframework.samples.petclinic.model.GeneratedValue;
+import org.springframework.samples.petclinic.model.Column;
+
+@Entity
+@Table(name = "videos")
+data class Video(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = false, length = 100)
+    val title: String,
+
+    @Column(nullable = false, unique = true)
+    val url: String,
+
+    @Column(nullable = false, length = 500)
+    val description: String,
+
+    @Column(nullable = true, length = 500)
+    val cast: String,
+
+    @Column()
+    val director: String,
+
+    @Column()
+    val rating: ,
+)
